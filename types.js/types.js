2bca8b89e676a8fdbbfd3897400e41999f4ada7e
vatiza <rakibur.mac@gmail.com>
2023-06-18 01:11:48 +0600

    code

---
+++ types.js/types.js
@@ -0,0 +1,15 @@
+const a=5;
+const b="samsu kopai na ekhon";
+const d=true;
+const ages=[34,321,54,32];
+const student={id:23,class:7}
+//console.log(typeof a ,typeof b,typeof ages,typeof student);
+let x=5;
+let y=x;
+console.log(x,y);
+y=7;
+console.log(x,y);
+ let p={job:'web development'}
+let q= p;
+q.job={job:'backend end'}
+console.log(p,q);
